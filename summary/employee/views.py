7d9ecafdbc3e4f2e3fa27f16465c31796bbedc38
Nadeem_Shah <48100883+Nadeem7hah@users.noreply.github.com>
2021-02-09 15:55:22 +0500

    get salary api added

--- summary/employee/views.py
+++ summary/employee/views.py
@@ -14,5 +14,30 @@ def get_employees(request):
 
     return Response(employee_obj_serialized, status=HTTP_200_OK)
 
+@api_view(['POST'])
+def create_employee(request):
+    first_name = request.data.get('first_name')
+    last_name = request.data.get('last_name')
+    username = request.data.get('username')
+    email = request.data.get('email')
+    department = request.data.get('department')
+    salary_per_day = request.data.get('salary_per_day')
+    total_working_days = request.data.get('total_working_days')
 
-# Create your views here.
+    Employee(None, first_name, last_name, username, email, department, salary_per_day, total_working_days).save()
+
+    response = {"STATUS": "SUCCESS"}
+
+    return Response(response, status=HTTP_200_OK)
+
+
+@api_view(['GET'])
+def get_salary(request, employee_id):
+    emp_obj = Employee.objects.get(pk=employee_id)
+
+    monthly_salary = int(emp_obj.salary_per_day) * int(emp_obj.total_working_days)
+
+    response = {"STATUS": "SUCCESS", "salary": monthly_salary}
+
+    return Response(response, status=HTTP_200_OK)
+
